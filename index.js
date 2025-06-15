7dc414b2d88e39a7348d51b92a791fb6fc4955b9
Tran Xuan Hiep <xuanhiep@win>
2025-06-15 12:55:22 +0700

    [add] nodejs

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
